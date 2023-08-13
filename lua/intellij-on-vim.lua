0b21c9d428e8858b3061bd916e22514a92cfcc50
Chau Bao Long <chau.bao.long.vn@gmail.com>
2023-08-13 22:06:55 +0700

    Add function to move cursor.

--- lua/intellij-on-vim.lua
+++ lua/intellij-on-vim.lua
@@ -5,4 +5,19 @@ M.setup = function(opt)
   require("intellij-on-vim.events").setup()
 end
 
+M.move_cursor = function(offset)
+  local mode = vim.api.nvim_get_mode().mode
+
+  if mode == "i" then
+    -- Caret position change event keep triggering when typing.
+    -- Don't move cursor in insert mode to avoid this annoying issue.
+    return
+  end
+
+  local line = vim.fn.byte2line(offset)
+  local col = offset - vim.fn.line2byte(line) + 2
+
+  vim.fn.cursor(line, col)
+end
+
 return M
